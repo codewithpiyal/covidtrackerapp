ccd3ed002b27f3457afd0c141e639b2dea7dfeab
Piyal Saha <piyallove2code@gmail.com>
2023-05-14 15:00:36 +0530

    Hourly_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState,useEffect} from 'react'
 
 function App() {
+  const[covidData,setCovidData]= useState("")
+     
+  useEffect(()=>{
+    fetch("https://data.covid19india.org/data.json")
+    .then(res=>res.json())
+    .then(data=>setCovidData(data))
+  },[covidData])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Live Covid Tracker</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Active</th>
+            <th>Confirmed</th>
+            <th>Deaths</th>
+            <th>StateCode </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            covidData.statewise.map((cElem)=>{
+              return(
+                <>
+                  <tr>
+                    <td>{cElem.active}</td>
+                    <td>{cElem.confirmed}</td>
+                    <td>{cElem.deaths}</td>
+                    <td>{cElem.statecode}</td>
+                  </tr>
+                </>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
